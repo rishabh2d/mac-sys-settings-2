cc104fd88a635f912f43b760a20effbdd1f4f752
rishabh <rishabh.sharma9102@gmail.com>
2026-04-07 00:24:33 -0700

    Initial Commit

---
+++ MICNMUSIC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MICNMUSIC
+//
+//  Created by Rishabh on 07/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MICNMUSIC/MICNMUSICApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICNMUSICApp.swift
+//  MICNMUSIC
+//
+//  Created by Rishabh on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MICNMUSICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
